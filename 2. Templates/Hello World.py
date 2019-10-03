6e75ed771ce0bf6677b669b3ca10c394ae2d303f
Amir Ali <amirali.cheema@outlook.com>
2019-10-03 09:03:42 +0500

    first commit

---
+++ 2. Templates/Hello World.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,  url_for
+app = Flask(__name__)
+
+posts = [
+      {
+         'author': 'Amir Ali',
+         'title': 'Blog Post 1',
+         'content': 'First post content',
+         'date_posted': 'April 20, 2018'
+      },
+      {
+         'author': 'Zain Amin',
+         'title': 'Blog Post 2',
+         'content': 'Second post content',
+         'date_posted': 'April 21, 2018'
+      }
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
